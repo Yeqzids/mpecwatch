e14048446a40055abf75883f51e9a9bc0308db08
Quanzhi Ye <qye@umd.edu>
2021-02-06 19:43:25 -0800

    just generate a plot

--- makepages/MPECTally.py
+++ makepages/MPECTally.py
@@ -2,15 +2,54 @@
 
 """
  PROJECT:		MPEC Watch
- PURPOSE:		generate a static HTML page, powered by plot.ly, that shows a bar chart and a table of all MPECs listed by year
+ PURPOSE:		Generate two plots:
+				(1) Types of MPECs vs year
+				(2) Object types of MPEC vs year
 
  (C) Quanzhi Ye
  
 """
 
-import sqlite3, plotly.express as px
+import sqlite3, plotly.express as px, pandas as pd, datetime, numpy as np
 
-dbFile = 'mpecwatch.db'
+dbFile = '../mpecwatch.db'
 
-fig =px.scatter(x=range(10), y=range(10))
-fig.write_html("path/to/file.html")
+db = sqlite3.connect(dbFile)
+cursor = db.cursor()
+
+df = pd.DataFrame({"Year": [], "MPECType": [], "#MPECs": []}) 
+df2 = pd.DataFrame({"Year": [], "ObjType": [], "#MPECs": []}) 
+
+for year in list(np.arange(1993, datetime.datetime.now().year+1, 1)):
+	cursor.execute("select * from MPEC where Time like '{}%' and MPECType = '{}'".format(year, 'Editorial'))
+	editorial = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and MPECType = '{}'".format(year, 'Discovery'))
+	discovery = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and MPECType = '{}'".format(year, 'OrbitUpdate'))
+	orbitupdate = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and MPECType = '{}'".format(year, 'DOU'))
+	dou = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and MPECType = '{}'".format(year, 'ListUpdate'))
+	listupdate = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and MPECType = '{}'".format(year, 'Other'))
+	other = len(cursor.fetchall())
+	
+	df = df.append(pd.DataFrame({"Year": [year, year, year, year, year, year], "MPECType": ["Editorial", "Discovery", "OrbitUpdate", "DOU", "ListUpdate", "Other"], "#MPECs": [editorial, discovery, orbitupdate, dou, listupdate, other]}), ignore_index = True)
+	
+	cursor.execute("select * from MPEC where Time like '{}%' and ObjectType = '{}'".format(year, 'NEA'))
+	nea = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and ObjectType = '{}'".format(year, 'Comet'))
+	comet = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and ObjectType = '{}'".format(year, 'Satellite'))
+	satellite = len(cursor.fetchall())
+	cursor.execute("select * from MPEC where Time like '{}%' and ObjectType = '{}'".format(year, 'Unusual'))
+	unusual = len(cursor.fetchall())
+	
+	df = df.append(pd.DataFrame({"Year": [year, year, year, year, year, year], "MPECType": ["Editorial", "Discovery", "OrbitUpdate", "DOU", "ListUpdate", "Other"], "#MPECs": [editorial, discovery, orbitupdate, dou, listupdate, other]}), ignore_index = True)
+
+fig = px.bar(df, x="Year", y="#MPECs", color="MPECType", title="Number and type of MPECs by year")
+fig.show()
+
+fig.write_html("MPECTally_ByYear_Fig.html")
+
+
