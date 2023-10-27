3439043bd1e2fb6ba1819f6441666093c3bfa9ae
Taegon Hibbitts <thibbitts03@gmail.com>
2023-10-27 10:57:28 -0400

    seasonal frequency figure

--- makepages/Individual_OMF.py
+++ makepages/Individual_OMF.py
@@ -1,4 +1,5 @@
 import sqlite3, plotly.express as px, pandas as pd, json
+from datetime import date
 
 mpecconn = sqlite3.connect("../mpecwatch_v3.db")
 cursor = mpecconn.cursor()
@@ -51,19 +52,49 @@ def topN(someDictionary, graphTitle, station, includeNA = False):
 
     fig1.write_html("../www/byStation/OMF/"+station+"_"+graphTitle.replace(' ', '_')+"{}.html".format(titleNA))
 
+def time_frequency_figure(station):
+    yearly = {'summer':0, 'autumn':0, 'winter':0, 'spring':0}
+    weekly = {'Monday':0, 'Tuesday':0, 'Wednesday':0, 'Thursday':0, 'Friday':0, 'Saturday':0, 'Sunday':0}
+    #daily = {} unable to use daily since data only includes the day of oberservation, no the time
+
+    cursor.execute("select Time from {}".format(station))
+    for time in cursor.fetchall():
+        time = int(time[0])
+        year = date.fromtimestamp(time).year
+        month = date.fromtimestamp(time).month
+        day = date.fromtimestamp(time).day
+        if month in [6,7,8]:
+            yearly['summer'] += 1
+        elif month in [9,10,11]:
+            yearly['autumn'] += 1
+        elif month in [12,1,2]:
+            yearly['winter'] += 1
+        elif month in [3,4,5]:
+            yearly['spring'] += 1
+        
+    df = pd.DataFrame(list(yearly.items()), columns=['Season', 'Count'])
+    print(df)
+    graph_title = station[-3:] + " " + mpccode[station[-3:]]['name']
+    fig1 = px.pie(df, values='Count', names='Season', title = graph_title + " | Seasonal Frequency")
+    fig1.write_html("../www/byStation/OMF/"+station+"_seasonal.html".replace(' ', '_'))
+
+
+
 N = 10 #Top limit of objects to show individually
 for station in mpccode.keys():
 #for i in range(1):
-    #station = "station_010"
+    #station = "station_J95"
     station = "station_" + station
     observers = {}
     measurers = {}
     facilities = {}
     try:
-        cursor.execute("select Observer, Measurer, Facility from {}".format(station))
+        cursor.execute("select Observer, Measurer, Facility, Time from {}".format(station))
+        message = "{} done".format(station)
     except:
-        print("Table {} does not exist".format(station))
-        pass
+        message = "Table {} does not exist".format(station)
+        
+        
     for mpec in cursor.fetchall():
         if (len(mpec[0]) > 30):
             observer = mpec[0][:30] + "..."
@@ -78,13 +109,16 @@ for station in mpccode.keys():
         topN(observers, "Top {} Observers".format(N), station)
         topN(measurers, "Top {} Measurers".format(N), station)
         topN(facilities, "Top {} Facilities".format(N), station)
+        time_frequency_figure(station)
+        #day week frequency
+
     except Exception as e:
-        print(e)
+        message = e
     
     #includes NA:
     #topN(observers, "Top {} Observers".format(N), station[0], True)
     
-    print(station + " done")
+    print(message)
     
 mpecconn.close()
 print('finished')
