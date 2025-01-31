2f858ef1c6e17e187bc4a8f50a9b617072ee4478
JAGIN410 <jhaegon123@gmail.com>
2025-01-31 03:18:54 -0500

    fixed overlapping navbar

--- makepages/StationMPECGraph.py
+++ makepages/StationMPECGraph.py
@@ -227,33 +227,32 @@ def make_station_page(station_code):
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">MPEC Watch</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/index.html">Home</a></li>
-                    <li class="active"><a href="https://sbnmpc.astro.umd.edu/mpecwatch/obs.html">Observatory Browser</a></li>
-                    <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/survey.html">Survey Browser</a></li>
-                    <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/stats.html">Various Statistics</a></li>
-                    <!-- <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/mpc_stuff.html">MPC Stuff (non-public)</a></li> -->
-                    <li><a href="https://github.com/Yeqzids/mpecwatch/issues">Issue Tracker</a></li>
-                    <li><a href="https://sbnmpc.astro.umd.edu">SBN-MPC Annex</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
+<!-- Fixed navbar -->
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">MPEC Watch</a>
         </div>
-    </nav>
-
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/index.html">Home</a></li>
+                <li class="active"><a href="https://sbnmpc.astro.umd.edu/mpecwatch/obs.html">Observatory Browser</a></li>
+                <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/survey.html">Survey Browser</a></li>
+                <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/stats.html">Various Statistics</a></li>
+                <!-- <li><a href="https://sbnmpc.astro.umd.edu/mpecwatch/mpc_stuff.html">MPC Stuff (non-public)</a></li> -->
+                <li><a href="https://github.com/Yeqzids/mpecwatch/issues">Issue Tracker</a></li>
+                <li><a href="https://sbnmpc.astro.umd.edu">SBN-MPC Annex</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+  </nav>
+  <body style="padding-top: 50px;">
     <div class="container theme-showcase" role="main">
         <div class="row">
             <!-- Main jumbotron for a primary marketing message or call to action -->
@@ -468,7 +467,7 @@ def make_station_page(station_code):
                 </tbody>
             </table>
         </div>
-        <footer class="pt-5 my-5 text-muted border-top">
+        <footer class="text-muted border-top">
         Script by <a href="https://www.astro.umd.edu/~qye/">Quanzhi Ye</a> and <a href="https://taegonhibbitts.com">Taegon Hibbitts</a>, hosted at <a href="https://sbnmpc.astro.umd.edu">SBN-MPC</a>. Powered by <a href="https://getbootstrap.com"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap-fill" viewBox="0 0 16 16">
         <path d="M6.375 7.125V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z"/>
         <path d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12V3.545h3.399c1.587 0 2.543.809 2.543 2.11 0 .884-.65 1.675-1.483 1.816v.1c1.143.117 1.904.931 1.904 2.033 0 1.488-1.084 2.396-2.888 2.396H5.062z"/>
@@ -519,37 +518,38 @@ def make_station_page(station_code):
 
     logging.info(f"Finished processing for station: {station_code}")
 
-make_station_page('004')
+# for testing a single station page
+#make_station_page('G96')
 
-# ES_CONTINUOUS = 0x80000000
-# ES_SYSTEM_REQUIRED = 0x00000001
+ES_CONTINUOUS = 0x80000000
+ES_SYSTEM_REQUIRED = 0x00000001
 
-# def prevent_sleep():
-#     ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
+def prevent_sleep():
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
 
-# def allow_sleep():
-#     ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
+def allow_sleep():
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
 
-# if __name__ == "__main__":
-#     prevent_sleep()
-#     try:
-#         time_start = datetime.datetime.now()
-#         signal.signal(signal.SIGINT, signal_handler)
-#         max_workers = os.cpu_count()
-#         with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-#             futures = {executor.submit(make_station_page, station_code): station_code for station_code in obscode}
-#             for future in concurrent.futures.as_completed(futures):
-#                 station_code = futures[future]
-#                 try:
-#                     future.result()
-#                 except Exception as exc:
-#                     print(f'Generated an exception: {exc}')
-#                     traceback.print_exc()
-#                     stop_event.set()
-#                     print(f'Error processing station: {station_code}')
-#         time_end = datetime.datetime.now()
-#         logging.info(f"Total time taken: {time_end - time_start}")
-#     finally:
-#         allow_sleep()
-#         mpecconn.close()
-#         logging.shutdown()
+if __name__ == "__main__":
+    prevent_sleep()
+    try:
+        time_start = datetime.datetime.now()
+        signal.signal(signal.SIGINT, signal_handler)
+        max_workers = os.cpu_count()
+        with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+            futures = {executor.submit(make_station_page, station_code): station_code for station_code in obscode}
+            for future in concurrent.futures.as_completed(futures):
+                station_code = futures[future]
+                try:
+                    future.result()
+                except Exception as exc:
+                    print(f'Generated an exception: {exc}')
+                    traceback.print_exc()
+                    stop_event.set()
+                    print(f'Error processing station: {station_code}')
+        time_end = datetime.datetime.now()
+        logging.info(f"Total time taken: {time_end - time_start}")
+    finally:
+        allow_sleep()
+        mpecconn.close()
+        logging.shutdown()
