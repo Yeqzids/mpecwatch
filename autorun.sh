5b6c2c92d79467fe864258082b77054bfb584241
Quanzhi Ye <qye@umd.edu>
2026-02-05 22:41:46 -0500

    update order

--- autorun.sh
+++ autorun.sh
@@ -1,4 +1,4 @@
-cd /home/qye/mpec
+cd /geminid1nb/qye/mpec
 python proc.py "$(date +'%Y%m')"
 python mpccode.py
 cd makepages
@@ -8,12 +8,12 @@ python home.py
 python obscode_stat.py
 python browser.py
 python mpc_stat.py
-python StationPage.py
 python Individual_OMF.py
+python StationPage.py
 python Overall_OMF.py
 python TopObjectsObs_PieChart.py
 python stats.py
 python survey.py
 python ObjectPage.py
 cp *.html ../www
-cp -r /home/qye/mpec/www/ /an/chiron~4/qye/mpecwatch
+cp -r /geminid1nb/qye/mpec/www/ /an/chiron~4/qye/mpecwatch
